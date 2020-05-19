bfb10ba60e62f42fbdaf4d36915c70cc84a08954
robelteklay92 <robielteklay92@gmail.com>
2020-05-19 14:31:33 +0100

    update read first letter case sensitive

--- dictbrnc.py
+++ dictbrnc.py
@@ -7,6 +7,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) >0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
