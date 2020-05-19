ee52a9202e6d771fdb62d51fef48b45af9d31a51
robelteklay92 <robielteklay92@gmail.com>
2020-05-19 14:19:34 +0100

    finalized dictionary app

--- dictbrnc.py
+++ dictbrnc.py
@@ -8,8 +8,24 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) >0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]  
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word does not exist. Please double check it!"
+        else:
+            return "Sorry we don't understand your entry!"
     else:
-        return "Word doesn't exist, please double check it!"
+        return "The Word doesn't exist. Please double check it!"
+
 word = input("Enter a word: ")
-print(translate(word))
+
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+
+
