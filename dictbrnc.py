c21e673d41b93e67dc6919e47849d56747ae2806
robelteklay92 <robielteklay92@gmail.com>
2020-05-19 13:13:04 +0100

    added a code to check similar words

--- dictbrnc.py
+++ dictbrnc.py
@@ -1,12 +1,15 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translate(word):
-    word = word.lower()
-    if word in data:
-        return data[word]
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) >0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]  
     else:
-        return "word doesn't exist, please double check!"
+        return "Word doesn't exist, please double check it!"
 word = input("Enter a word: ")
 print(translate(word))
