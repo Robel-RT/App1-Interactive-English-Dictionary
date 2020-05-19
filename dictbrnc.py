696e37755fbb2ce1172f3e36570d0cdd1a0ea727
robelteklay92 <robielteklay92@gmail.com>
2020-05-19 14:34:42 +0100

    Finalizing App 1

--- dictbrnc.py
+++ dictbrnc.py
@@ -9,6 +9,8 @@ def translate(w):
         return data[w]
     elif w.title() in data:
         return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) >0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
